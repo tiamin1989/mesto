4b5ef236b2e40031ca7cc69bc056b85bd29575c5
Ruslan Ramazanov <tiamin1989@yandex.ru>
2020-05-08 09:41:41 +0500

    refactor popup js

--- pages/script.js
+++ pages/script.js
@@ -1,34 +1,12 @@
 const titles = document.querySelectorAll('.photo-grid__title');
-
 const personProfile = document.querySelector('.profile__person');
 const personName = document.querySelector('.popup__name');
 const personActivity = document.querySelector('.popup__activity');
-
-// Загрузим имя и деятельность персоны в popup
-personName.value = personProfile.querySelector('.profile__name').textContent;
-personActivity.value = personProfile.querySelector('.profile__activity').textContent;
-
-// Добавим поведение закрытия по кнопке для popup
 const closePopup = document.querySelector('.popup__close');
-closePopup.addEventListener('click', function () {
-  document.querySelector('.popup').style = 'display: none;'
-});
-
-// Добавим поведение открытия popup по кнопке редактирования
 const profileEdit = document.querySelector('.profile__edit');
-profileEdit.addEventListener('click', function () {
-  document.querySelector('.popup').style = 'display: block;';
-});
+const popupForm = document.querySelector('.popup__container');
 
-// Укоротим длинные заголовки
-for (let i = 0; i < titles.length; i++) {
-  if (titles[i].textContent.length > 12) {
-    titles[i].textContent = titles[i].textContent.substring(0, 11) + '...';
-  }
-}
-
-const formElement = document.querySelector('.popup__container');
-
+// Сохранение данных по кнопке Сохранить
 function formSubmitHandler(evt) {
   evt.preventDefault();
   personProfile.querySelector('.profile__name').textContent = personName.value;
@@ -36,4 +14,28 @@ function formSubmitHandler(evt) {
   document.querySelector('.popup').style = 'display: none;';
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+// Поведение при открытии редактирования персоны и закрытии popup
+function popupLoad() {
+  if (document.querySelector('.popup').style.display === 'none') {
+    // Загрузим имя и деятельность персоны в popup
+    personName.value = personProfile.querySelector('.profile__name').textContent;
+    personActivity.value = personProfile.querySelector('.profile__activity').textContent;
+    document.querySelector('.popup').style.display = 'block';
+  } else document.querySelector('.popup').style.display = 'none';
+}
+
+// Добавим поведение закрытия по кнопке для popup
+closePopup.addEventListener('click', popupLoad);
+
+// Добавим поведение открытия popup по кнопке редактирования
+profileEdit.addEventListener('click', popupLoad);
+
+// Поведение при нажатии Сохранить для popup
+popupForm.addEventListener('submit', formSubmitHandler);
+
+// Укоротим длинные заголовки
+for (let i = 0; i < titles.length; i++) {
+  if (titles[i].textContent.length > 12) {
+    titles[i].textContent = titles[i].textContent.substring(0, 11) + '...';
+  }
+}
