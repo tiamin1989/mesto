7fbf43869009a7107ae34d6f3b5f8ec47f9fda10
Ruslan Ramazanov <tiamin1989@yandex.ru>
2020-05-23 22:28:00 +0500

    added js prefend cards

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,32 @@ const personActivity = document.querySelector('.popup__activity');
 const closePopup = document.querySelector('.popup__close');
 const profileEdit = document.querySelector('.profile__edit');
 const popupForm = document.querySelector('.popup__container');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
 // Сохранение данных по кнопке Сохранить
 function formSubmitHandler(evt) {
@@ -15,19 +41,34 @@ function formSubmitHandler(evt) {
 }
 
 // Поведение при открытии редактирования персоны и закрытии popup
-function popupLoad() {
+function popupShow() {
   personName.value = personProfile.querySelector('.profile__name').textContent;
   personActivity.value = personProfile.querySelector('.profile__activity').textContent;
   popupElement.classList.add('popup_opened');
 }
 
-// Добавим поведение закрытия по кнопке для popup
+// Добавление карточек из массива
+function addCards(...cards) {console.log(cards)
+  cards.forEach((object) => {
+    const cardTemplate = document.querySelector('#photo').content;
+    const card = cardTemplate.cloneNode(true);
+    card.querySelector('.photo-grid__photo').setAttribute('src', object.link);
+    card.querySelector('.photo-grid__photo').setAttribute('alt', object.name);
+    card.querySelector('.photo-grid__title').textContent = object.name;
+    document.querySelector('.photo-grid').prepend(card);
+  }
+  );
+}
+
+// Поведение закрытия по кнопке для popup
 closePopup.addEventListener('click', () => {
   popupElement.classList.remove('popup_opened');
 });
 
-// Добавим поведение открытия popup по кнопке редактирования
-profileEdit.addEventListener('click', popupLoad);
+// Поведение открытия popup по кнопке редактирования
+profileEdit.addEventListener('click', popupShow);
 
 // Поведение при нажатии Сохранить для popup
 popupForm.addEventListener('submit', formSubmitHandler);
+
+addCards(...initialCards);
