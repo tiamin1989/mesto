4a3663977db67c372fd508b86b45b1c1fed79cf4
Ruslan Ramazanov <tiamin1989@yandex.ru>
2020-05-26 23:18:41 +0500

    some fixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,32 @@
-const personProfile = document.querySelector('.profile__person');
-const cardElement = document.querySelector('#card');
-const profileElement = document.querySelector('#profile');
+const personProfile = { 
+  name: document.querySelector('.profile__name'),
+  activity: document.querySelector('.profile__activity')
+}
+
+const cardElement = {
+  block: document.querySelector('#card'),
+  name: document.querySelector('#card .popup__name'),
+  activity: document.querySelector('#card .popup__activity'),
+  close: document.querySelector('#card .popup__close')
+}
+
+const profileElement = {
+  block: document.querySelector('#profile'),
+  name: document.querySelector('#profile .popup__name'),
+  activity: document.querySelector('#profile .popup__activity'),
+  close: document.querySelector('#profile .popup__close')
+}
+
 const profileEdit = document.querySelector('.profile__edit');
 const profileAdd = document.querySelector('.profile__add');
-const photoPopup = document.querySelector('.photo-popup');
+
+const photoPopup = {
+  block: document.querySelector('.photo-popup'),
+  image: document.querySelector('.photo-popup__image'),
+  caption: document.querySelector('.photo-popup__caption'),
+  close: document.querySelector('.photo-popup__close')
+}
+
 const initialCards = [
   {
     name: 'Архыз',
@@ -34,9 +57,9 @@ const initialCards = [
 // Сохранение данных по кнопке Сохранить
 function saveProfile(evt) {
   evt.preventDefault();
-  personProfile.querySelector('.profile__name').textContent = profileElement.querySelector('.popup__name').value;
-  personProfile.querySelector('.profile__activity').textContent = profileElement.querySelector('.popup__activity').value;
-  profileElement.classList.remove('popup_opened');
+  personProfile.name.textContent = profileElement.name.value;
+  personProfile.activity.textContent = profileElement.activity.value;
+  profileElement.block.classList.remove('popup_opened');
 }
 
 // Добавление карточек из массива и навешивание обработчиков
@@ -48,10 +71,10 @@ function addCards(...cards) {
     card.querySelector('.photo-grid__photo').setAttribute('src', object.link);
     card.querySelector('.photo-grid__photo').setAttribute('alt', object.name);
     card.querySelector('.photo-grid__photo').addEventListener('click', evt => {
-      photoPopup.querySelector('.photo-popup__image').setAttribute('src', evt.target.getAttribute('src'));
-      photoPopup.querySelector('.photo-popup__image').setAttribute('alt', evt.target.getAttribute('alt'));
-      photoPopup.querySelector('.photo-popup__caption').textContent = evt.target.getAttribute('alt');
-      photoPopup.classList.add('photo-popup_opened');
+      photoPopup.image.setAttribute('src', evt.target.getAttribute('src'));
+      photoPopup.image.setAttribute('alt', evt.target.getAttribute('alt'));
+      photoPopup.caption.textContent = evt.target.getAttribute('alt');
+      photoPopup.block.classList.add('photo-popup_opened');
     });
     card.querySelector('.photo-grid__title').textContent = object.name;
     card.querySelector('.photo-grid__heart').addEventListener('click', evt => evt.target.classList.toggle('photo-grid__heart_liked'));
@@ -61,47 +84,47 @@ function addCards(...cards) {
 
 // Поведение при открытии редактирования персоны
 function editProfilePopup() {
-  profileElement.querySelector('.popup__name').value = personProfile.querySelector('.profile__name').textContent;
-  profileElement.querySelector('.popup__activity').value = personProfile.querySelector('.profile__activity').textContent;
-  profileElement.classList.add('popup_opened');
+  profileElement.name.value = personProfile.name.textContent;
+  profileElement.activity.value = personProfile.activity.textContent;
+  profileElement.block.classList.add('popup_opened');
 }
 
-// Поведение открытия popup по кнопке добавления
+// Поведение открытия popup по кнопке добавления (******button type="reset" не работает******)
 function newCardPopup() {
-  cardElement.querySelector('.popup__name').value = '';
-  cardElement.querySelector('.popup__activity').value = '';
-  cardElement.classList.add('popup_opened');
+  cardElement.name.value = '';
+  cardElement.activity.value = '';
+  cardElement.block.classList.add('popup_opened');
 }
 
 profileEdit.addEventListener('click', editProfilePopup);
 profileAdd.addEventListener('click', newCardPopup);
 
 // Закрытие по по крестику добавления карточки
-cardElement.querySelector('.popup__close').addEventListener('click', evt => {
+cardElement.close.addEventListener('click', evt => {
   evt.preventDefault();
-  cardElement.classList.remove('popup_opened');
+  cardElement.block.classList.remove('popup_opened');
 });
 
 // Закрытие по по крестику редактировани профиля
-profileElement.querySelector('.popup__close').addEventListener('click', evt => {
+profileElement.close.addEventListener('click', evt => {
   evt.preventDefault();
-  profileElement.classList.remove('popup_opened');
+  profileElement.block.classList.remove('popup_opened');
 });
 
 // Закрытие по по крестику фотографии
-photoPopup.querySelector('.photo-popup__close').addEventListener('click', evt => photoPopup.classList.remove('photo-popup_opened'));
+photoPopup.close.addEventListener('click', evt => photoPopup.block.classList.remove('photo-popup_opened'));
 
 // Создание новой карточки по кнопке Создать
-cardElement.addEventListener('submit', evt => {
+cardElement.block.addEventListener('submit', evt => {
   evt.preventDefault();
   addCards({
-    name: cardElement.querySelector('.popup__name').value,
-    link: cardElement.querySelector('.popup__activity').value
+    name: cardElement.name.value,
+    link: cardElement.activity.value
   });
-  cardElement.classList.remove('popup_opened');
+  cardElement.block.classList.remove('popup_opened');
 });
 
 // Редактирование профиля по кнопке Сохранить
-profileElement.addEventListener('submit', saveProfile);
+profileElement.block.addEventListener('submit', saveProfile);
 
 addCards(...initialCards);
